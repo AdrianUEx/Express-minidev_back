50d3d5fb30d4840dadecfe805a290431a1fa7f68
Adrian <abarquilx@alumnos.unex.es>
2026-09-25 17:58:49 +0200

    Aplicación del Barrel Pattern en /repositories/typeorm. Corrección en book.ts de la libreria typeorm/browser a typeorm.

---
+++ src/infrastructure/repositories/typeorm/index.ts
@@ -0,0 +1,5 @@
+// Barrel pattern for TypeORM Repositories
+export * from "./authorRepository";
+export * from "./bookRepository";
+export * from "./customerRepository";
+export * from "./loanRepository";
